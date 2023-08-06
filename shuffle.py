0acf8aac3bdd0514b2d7edf61baee1c95b40dbfa
ilike3pancakes <ilike3pancakes@gmail.com>
2023-08-06 22:57:42 +0100

    switch to subprocess

--- shuffle.py
+++ shuffle.py
@@ -1,13 +1,15 @@
 
-from sh import bash
+import subprocess
 
 
 def candidate() -> str:
     """
     Returns a random american english dictionary word, capitalised.
     """
+    command = """cat /usr/share/dict/american-english | shuf | grep -v "'s" | head -n 1"""
+    result = subprocess.run(['/bin/bash', '-c', command], capture_output=True, text=True)
 
-    return bash("-c", """cat /usr/share/dict/american-english | shuf | grep -v "'s" | head -n 1""").capitalize()
+    return result.stdout.capitalize()
 
 
 if __name__ == "__main__":
