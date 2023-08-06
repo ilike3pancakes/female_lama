a09c8f202a42dd07d6256c8c893e4dba840019e1
ilike3pancakes <ilike3pancakes@gmail.com>
2023-08-06 22:59:34 +0100

    debug

--- shuffle.py
+++ shuffle.py
@@ -9,7 +9,11 @@ def candidate() -> str:
     command = """cat /usr/share/dict/american-english | shuf | grep -v "'s" | head -n 1"""
     result = subprocess.run(['/bin/bash', '-c', command], capture_output=True, text=True)
 
-    return result.stdout.capitalize()
+    word = result.stdout.capitalize()
+
+    print(f"Returning {word}")
+
+    return word
 
 
 if __name__ == "__main__":
