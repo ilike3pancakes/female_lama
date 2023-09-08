c0e71b4648913dae3f77d0e23103bda962f43fe9
ilike3pancakes <ilike3pancakes@gmail.com>
2023-09-08 22:38:13 +0100

    log persistence

--- persistence.py
+++ persistence.py
@@ -1,15 +1,21 @@
 
 from dataclasses import asdict
+from pathlib import Path
 import typing
 from typing import Callable
 import yaml
+import logging
+
+logger = logging.getLogger()
 
 
 class PersistenceMixin:
     def write(self, where: str) -> None:
-        with open(where, "w") as f:
+        path = Path(where)
+        with open(path, "w") as f:
             # https://github.com/python/mypy/issues/14941
             f.write(yaml.dump(asdict(self)))  # type: ignore
+            logger.info(f"Wrote to {path.absolute}")
 
     @classmethod
     def read(cls, where: str, *, default_ctor: Callable[[], typing.Any]) -> typing.Any:
