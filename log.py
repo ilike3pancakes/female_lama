7e8ae1575e881602dbb43ad5e8728e1dd5dba691
ilike3pancakes <ilike3pancakes@gmail.com>
2024-02-27 22:02:28 +0000

    vn++

--- log.py
+++ log.py
@@ -7,7 +7,7 @@ from kik_unofficial.client import KikClient  # type: ignore
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 stream_handler = logging.StreamHandler(sys.stdout)
-stream_handler.setFormatter(logging.Formatter(KikClient.log_format()))
+# stream_handler.setFormatter(logging.Formatter(KikClient.log_format()))
 logger.addHandler(stream_handler)
 
 def get_logger():
